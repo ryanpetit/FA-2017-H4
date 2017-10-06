070a533e897d793e79b884ce5519758730ca4f7f
student <student@ssu-cs-dar25-167-147.sonoma.edu>
2017-10-05 20:34:53 -0700

    working code complete

--- app/src/main/java/com/fa17/ssu385/fa_2017_h4/utilities/RecipeParser.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h4/utilities/RecipeParser.java
@@ -11,15 +11,26 @@ public class RecipeParser {
     public static final RecipeModel recipeFromJson(String jsonString) {
         RecipeModel model = null;
         try {
+            // -- Deserializing JSON --
+            // take character string of JSON response and make into JSON string assigned to response
             JSONObject response = new JSONObject(jsonString);
+            // looks within JSON object (response) for an array called "matches" and assign to matches
             JSONArray matches = response.getJSONArray("matches");
+            // grab object at index 0 within matches array, assign it to recipe
             JSONObject recipe = matches.getJSONObject(0);
 
             model = new RecipeModel();
+            // looks within the recipe object to find the value in the object recipeName and sets the name
             model.setRecipeName(recipe.getString("recipeName"));
+
+            JSONArray recipeImageUrl = recipe.getJSONArray("smallImageUrls");
+            model.setRecipeImageUrl(recipeImageUrl.getString(0));
+
         } catch (JSONException e) {
             // do something useful with exception
         }
         return model;
     }
 }
+
+// to get "smallImageUrls", need to create new JSON array, grab string at index 0
