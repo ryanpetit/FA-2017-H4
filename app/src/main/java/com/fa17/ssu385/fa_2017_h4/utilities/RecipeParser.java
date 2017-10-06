49ded8a929b85c3bae5f6f91fd12d93681a7214b
ezquire <tyler.gearing@gmail.com>
2017-10-06 09:12:30 -0700

    working code complete

--- app/src/main/java/com/fa17/ssu385/fa_2017_h4/utilities/RecipeParser.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h4/utilities/RecipeParser.java
@@ -15,8 +15,13 @@ public class RecipeParser {
             JSONArray matches = response.getJSONArray("matches");
             JSONObject recipe = matches.getJSONObject(0);
 
+            JSONArray smallImageUrls = response.getJSONArray("smallImageUrls");
+            JSONObject recipeImageUrl = smallImageUrls.getJSONObject(0);
+
             model = new RecipeModel();
             model.setRecipeName(recipe.getString("recipeName"));
+            model.setRecipeImageUrl(recipeImageUrl.getString("recipeImageUrl"));
+
         } catch (JSONException e) {
             // do something useful with exception
         }
