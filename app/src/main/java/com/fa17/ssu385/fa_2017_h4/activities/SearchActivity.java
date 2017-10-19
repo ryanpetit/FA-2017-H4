847d5e278261d57fc74e539a91c717b59aaee74a
Hayley Gerard <hayleygerard@wrls-102-151.sonoma.edu>
2017-10-18 18:19:53 -0700

    completed working homework

--- app/src/main/java/com/fa17/ssu385/fa_2017_h4/activities/SearchActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h4/activities/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.fa17.ssu385.fa_2017_h4.activities;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fa17.ssu385.fa_2017_h4.R;
+import com.fa17.ssu385.fa_2017_h4.models.RecipeModel;
+import com.fa17.ssu385.fa_2017_h4.network.RecipeCallbackListener;
 import com.fa17.ssu385.fa_2017_h4.network.RecipeSearchAsyncTask;
 
 public class SearchActivity extends AppCompatActivity {
@@ -17,11 +20,33 @@ public class SearchActivity extends AppCompatActivity {
     private ImageView recipeThumbnail;
     private EditText searchEditText;
     private Button searchButton;
+    RecipeCallbackListener recipeCallbackListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        recipeName = (TextView)findViewById(R.id.recipe_name);
+        recipeThumbnail = (ImageView)findViewById(R.id.recipe_thumbnail);
+        searchEditText = (EditText)findViewById(R.id.search_edit_text);
+        searchButton = (Button)findViewById(R.id.my_search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                recipeCallbackListener = new RecipeCallbackListener() {
+                    @Override
+                    public void onRecipeCallback(RecipeModel recipeModel) {
+                        recipeName.setText(recipeModel.getRecipeName());
+                    }
+                };
+                RecipeSearchAsyncTask task = new RecipeSearchAsyncTask();
+                task.setRecipeCallbackListener(recipeCallbackListener);
+                task.execute(searchEditText.getText().toString());
+            }
+
+
+        });
 
     }
 }
