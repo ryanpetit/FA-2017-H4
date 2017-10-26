316bd6013b97534680131ffbb1a5de6066852523
ngiaccio <ngiaccio@gmail.com>
2017-10-26 12:56:47 -0700

    homework complete

--- app/src/main/java/com/fa17/ssu385/fa_2017_h4/models/RecipeModel.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h4/models/RecipeModel.java
@@ -3,6 +3,18 @@ package com.fa17.ssu385.fa_2017_h4.models;
 
 public class RecipeModel {
     private String recipeName;
+    private String recipeImageUrl;
+    private String recipeDescription;
+
+    public String getRecipeImageUrl() { return recipeImageUrl; }
+
+    public void setRecipeImageUrl(String recipeImageUrl) { this.recipeImageUrl = recipeImageUrl; }
+
+
+    public String getRecipeDescription() { return recipeDescription; }
+
+    public void setRecipeDescription(String recipeDescription) { this.recipeDescription = recipeDescription; }
+
 
     public String getRecipeName() {
         return recipeName;
@@ -11,4 +23,5 @@ public class RecipeModel {
     public void setRecipeName(String recipeName) {
         this.recipeName = recipeName;
     }
+
 }
--- app/src/main/java/com/fa17/ssu385/fa_2017_h4/network/RecipeSearchAsyncTask.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h4/network/RecipeSearchAsyncTask.java
@@ -2,6 +2,7 @@ package com.fa17.ssu385.fa_2017_h4.network;
 
 import android.os.AsyncTask;
 
+import com.fa17.ssu385.fa_2017_h4.activities.SearchActivity;
 import com.fa17.ssu385.fa_2017_h4.models.RecipeModel;
 import com.fa17.ssu385.fa_2017_h4.utilities.RecipeParser;
 import com.squareup.okhttp.HttpUrl;
@@ -13,7 +14,6 @@ import java.io.IOException;
 
 
 public class RecipeSearchAsyncTask extends AsyncTask<String, String, RecipeModel> {
-
     private String baseApiUrl = "http://api.yummly.com/v1/api/recipes";
     private String apiKey = "ec3e34e0bb6801670dbd3dbd02ce7608";
     private String appId = "4911b643";
@@ -22,18 +22,32 @@ public class RecipeSearchAsyncTask extends AsyncTask<String, String, RecipeModel
 
     @Override
     protected RecipeModel doInBackground(String... params) {
-
+        String searchParams = params[0];
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(baseApiUrl).newBuilder();
+        urlBuilder.addQueryParameter("_app_key", apiKey);
+        urlBuilder.addQueryParameter("_app_id", appId);
+        urlBuilder.addQueryParameter("your_search_parameters", searchParams);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(url).build();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            if (response != null) {
+                return RecipeParser.recipeFromJson(response.body().string());
+            }
+        } catch (IOException e) {
+            // do something with exception
+        }
         return null;
     }
-
     @Override
     protected void onPostExecute(RecipeModel recipeModel) {
         super.onPostExecute(recipeModel);
-
+        recipeCallbackListener.onRecipeCallback(recipeModel);
     }
 
     public void setRecipeCallbackListener(RecipeCallbackListener recipeCallbackListener) {
         this.recipeCallbackListener = recipeCallbackListener;
     }
-
 }
